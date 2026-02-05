4cb9ae97809ee85feeb49cea387c59eeed807c2d
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-02-05 20:50:03 +0800

    mi_camera_holder: init

---
+++ models/mi_camera_holder.py
@@ -0,0 +1,90 @@
+# %% [markdown]
+# Exhaust Vent Holder
+
+# %%
+
+from decimal import Decimal
+import typing
+from build123d import *
+from ocp_vscode import *
+import math
+import os
+import sys
+
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
+f = float
+
+# %%
+
+def show_list(lst, locals, camera=Camera.KEEP):
+    lst = lst[:]
+    if "test" in locals:
+        lst.append(locals["test"])
+    show(*lst, reset_camera=camera)
+
+# %%
+
+accuracy = 0.2
+screw_hole_size = 2.6
+screw_pillar_size = 9.0
+screw_pillar_height = 15.0
+hole_center_distance = 36.0
+radius = 50.0 / 2
+pole_diameter = 8.08
+hook_radius = pole_diameter / 2 + accuracy
+hook_width = 40.0
+hook_height = 20.0
+hook_thickness = 3.0
+base_thickness = 5.0
+hook_length = 5.0
+outer_shift = 15.0
+
+with BuildPart() as holder_builder:
+    # hook and arm
+    with Locations(
+        Location(Pos(-radius, radius, hook_height)),
+        Location(Pos(radius, -radius, hook_height) * Rotation(Z=-90))
+    ):
+        with BuildSketch(Plane.YZ):
+            slot_arc_radius = hook_radius + hook_thickness / 2
+            SlotArc(CenterArc((0, slot_arc_radius), slot_arc_radius, 270, -225), hook_thickness)
+        extruded = extrude(amount=hook_width / 2, both=True, mode=Mode.PRIVATE)
+        add(extruded)
+        with BuildSketch(Plane.XY):
+            Rectangle(hook_width, hook_thickness)
+        extruded = extrude(amount=-hook_height, mode=Mode.PRIVATE)
+        add(extruded)
+
+    with BuildSketch(Plane.XY) as test:
+        with BuildLine():
+            begin1 = (-radius - hook_width / 2, radius - hook_thickness / 2)
+            l1 = Line(begin1, (begin1[0] + hook_width, begin1[1]))
+            begin2 = (radius - hook_thickness / 2, -radius - hook_width / 2)
+            l2 = Line(begin2, (begin2[0], begin2[1] + hook_width))
+            l3 = Line(l1 @ 0, l2 @ 0)
+            l4 = Line(l1 @ 1, l2 @ 1)
+        make_face()
+    extrude(amount=base_thickness)
+
+    with Locations((-outer_shift, -outer_shift)):
+        Cylinder(radius, base_thickness, align=(Align.CENTER, Align.CENTER, Align.MIN))
+        with Locations(Rotation(Z=0)):
+            with GridLocations(
+                hole_center_distance,
+                0,
+                2,
+                1
+            ):
+                Cylinder(screw_pillar_size / 2, screw_pillar_height, align=(Align.CENTER, Align.CENTER, Align.MIN))
+                Cylinder(screw_hole_size / 2, screw_pillar_height, align=(Align.CENTER, Align.CENTER, Align.MIN), mode=Mode.SUBTRACT)
+
+show(holder_builder, test, reset_camera=Camera.KEEP)
+
+# %%
+
+from pathlib import Path
+
+script_directory = Path(__file__).resolve().parent
+export_stl(holder_builder.part, f"{script_directory}/../outputs/camera_holder.stl")
