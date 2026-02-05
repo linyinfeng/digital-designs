1451d30750aa5e93e0abb00a279902a49cf80e13
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-02-05 20:53:05 +0800

    wire-holder: init

---
+++ models/wire_holder.py
@@ -0,0 +1,57 @@
+# %% [markdown]
+# Exhaust Vent Holder
+
+# %%
+
+from decimal import Decimal
+import typing
+from build123d import *
+from ocp_vscode import *
+import math
+import os
+import sys
+
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
+f = float
+
+# %%
+
+def show_list(lst, locals, camera=Camera.KEEP):
+    lst = lst[:]
+    if "test" in locals:
+        lst.append(locals["test"])
+    show(*lst, reset_camera=camera)
+
+# %%
+
+accuracy = 0.2
+pole_diameter = 8.08
+hook_radius = pole_diameter / 2 + accuracy
+hook_width = 10.0
+hook_thickness = 2.0
+hook_arc_size = 270
+
+with BuildPart() as holder_builder:
+    slot_arc_radius = hook_radius + hook_thickness / 2
+    l1 = CenterArc((0, 0), slot_arc_radius, -hook_arc_size / 2, hook_arc_size, mode=Mode.PRIVATE)
+    l2 = CenterArc((slot_arc_radius, 0), slot_arc_radius, -hook_arc_size / 2, 330, mode=Mode.PRIVATE)
+    cut_x = l1.intersect(l2)[0].X
+    with BuildSketch():
+        SlotArc(l1, hook_thickness)
+    extrude(amount=hook_width / 2, both=True)
+    split(bisect_by=Plane.YZ.offset(cut_x), keep=Keep.BOTTOM)
+    with BuildSketch():
+        SlotArc(l2, hook_thickness)
+    extruded = extrude(amount=hook_width / 2, both=True, mode=Mode.PRIVATE)
+    split(extruded, bisect_by=Plane.YZ.offset(cut_x), keep=Keep.TOP, mode=Mode.ADD)
+
+show(holder_builder, reset_camera=Camera.KEEP)
+
+# %%
+
+from pathlib import Path
+
+script_directory = Path(__file__).resolve().parent
+export_stl(holder_builder.part, f"{script_directory}/../outputs/wire_holder.stl")
